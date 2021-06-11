93c3095744b14a040897f25285476d1ca3f48233
coolsvap <coolsvap@gmail.com>
2021-06-11 17:01:16 +0530

    Ingress specs updated

--- ingress/setup_ingress.sh
+++ ingress/setup_ingress.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.26.2/deploy/static/mandatory.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.26.2/deploy/static/provider/baremetal/service-nodeport.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.47.0/deploy/static/provider/baremetal/deploy.yaml
